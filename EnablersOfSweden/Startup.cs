7b0e49bf21488e3b2ff8ae241adb5be71e4b8fe6
aalhinai <aalhinai83@gmail.com>
2017-09-05 14:25:56 +0200

    init

---
+++ EnablersOfSweden/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EnablersOfSweden.Startup))]
+namespace EnablersOfSweden
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
